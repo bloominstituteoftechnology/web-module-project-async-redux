8b3bfb3ba913e82896dd5d79e098526dd3119302
Molly Brown <mollybrown0516@gmail.com>
2021-04-27 15:53:39 -0400

    Completed setup on index.js

---
+++ my-redux-project/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { applyMiddleware, createStore } from "redux";
+import { logger } from "redux-logger";
+import { Provider } from "react-redux";
+import { gotReducer } from "./reducers/gotReducer"; // My reducer will be named gotReducer, file name gotReducer.js
+import thunk from "redux-thunk";
+
+const store = createStore(gotReducer, applyMiddleware(logger, thunk));
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, rootElement)
+
+//THIS PAGE SHOULD BE COMPLETE IN SETUP
