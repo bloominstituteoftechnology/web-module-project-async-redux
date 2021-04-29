f99dc9fd297a6f27d5f16041d9f2a05daacefee1
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-28 21:52:39 -0500

    experimental image cache

--- app/src/components/PokeGrid.js
+++ app/src/components/PokeGrid.js
@@ -1,10 +1,39 @@
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import { mapStateToProps } from "../helpers/mapStateToProps";
 import { fetchUrlPokemon } from "../store/actions";
 import Pagination from "./Pagination";
 import PokeCard from "./PokeCard";
 
-const PokeGrid = ({ pokemonList, urlPokemon }) => {
+const PokeGrid = ({ pokemonList, urlPokemon, nextUrlPokemon }) => {
+  const nextPokemon = nextUrlPokemon.results.filter((nextPkmn) =>
+    pokemonList.find((pkmn) => pkmn.name === nextPkmn.name)
+  );
+
+  const preloadImages = async (imgsArray) => {
+    debugger;
+    const loadPromises = await imgsArray.map((imgSrc) => {
+      return new Promise(function (resolve, reject) {
+        const img = new Image();
+
+        img.alt = imgSrc.name;
+        img.src = imgSrc.sprites.other["official-artwork"].front_default;
+
+        img.onload = resolve();
+        img.onerror = reject();
+      });
+    });
+
+    await Promise.all(loadPromises);
+  };
+
+  useEffect(() => {
+    preloadImages(nextPokemon);
+  }, [nextPokemon]);
+  /*
+  If the img exists, use it 
+   */
+
   return (
     <section className="container">
       <div className="poke-grid">
@@ -13,12 +42,19 @@ const PokeGrid = ({ pokemonList, urlPokemon }) => {
             <PokeCard key={pkmn.id} pkmn={pkmn} />
           ) : null
         )}
+        {/* {pokemonList.map((pkmn) =>
+          nextUrlPokemon.results.find(
+            (urlPkmn) => urlPkmn.name === pkmn.name
+          ) ? (
+            <PokeCard key={pkmn.id} pkmn={pkmn} />
+          ) : null
+        )} */}
       </div>
       <Pagination onDisplayCount={urlPokemon.length} />
     </section>
   );
 };
 
-const mapState = mapStateToProps("pokemonList", "urlPokemon");
+const mapState = mapStateToProps("pokemonList", "urlPokemon", "nextUrlPokemon");
 
 export default connect(mapState, { fetchUrlPokemon })(PokeGrid);
