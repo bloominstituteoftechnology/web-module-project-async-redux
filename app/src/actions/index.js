07d55ffe5668b9b3efce7e1b28b1d445c6432c0f
cynthiaannberg <cynthia.ann.berg@me.com>
2021-05-12 17:44:19 -0600

    redux

---
+++ app/src/actions/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+
+
+export const fetchPerson = () => dispatch => {
+    dispatch({type: FETCH_START});
+
+    dispatch(fetchStart());
+    axios.get('https://randomuser.me/api/')
+        .then(resp=> {
+        dispatch({type: FETCH_SUCCESS, payload:resp.data.results[0]});
+        })
+        .catch(err=>{
+        dispatch({type: FETCH_FAIL, payload:err});
+        })
+}
+
+export const fetchStart = ()=> {
+    return({type: FETCH_START});
+}
+
+export const fetchSuccess = (person)=> {
+    return({type: FETCH_SUCCESS, payload:person});
+}
+
+export const fetchFail = (error)=> {
+    return({type: FETCH_FAIL, payload: error})
+}
