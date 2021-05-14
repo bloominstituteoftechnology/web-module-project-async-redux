c91f0f194fdf21bd9e33f0513d0d7997542a7d3a
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-05-13 21:43:10 -0600

    working on MVP

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import { connect } from 'react-dom'
+import CharacterList from './components/CharacterList'
 
-function App() {
+function App(props) {
+
+  const { characters, loading } = props;
+
   return (
     <div className="App">
-     
+     <h1>Use the force, Harry! -Gandolf</h1> 
+
+     {
+       loading ? <h3>Grabbing the data</h3> : <CharacterList />
+     }
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return{
+    lading:state.loading
+  }
+}
+
+export default connect(mapStateToProps)(App);
