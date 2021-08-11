f70d368d9a02d22c5ca446d14ac4fbbcd43fd328
Bradford-Meziere <coleokc@gmail.com>
2021-08-11 15:04:36 -0500

    Yeezus Quote Gen

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './index'
+import Quote from './Components/yeComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Godly Advice:</h1>
+      <Quote/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/Components/yeComponent.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { getQuote } from '../Actions/yeIndex';
+
+const Quote =(props)=>{
+    const { quote, isFetching, error} = props;
+
+    useEffect(()=>{
+        props.getQuote();
+    }, []);
+
+    if (error) {
+        return <h2>Error: {error} </h2>;
+    }
+
+    if (isFetching){
+        return <h2> Fetching quote from the lord!</h2>;
+    }
+
+    const handleClick=()=>{
+        props.getQuote();
+    }
+
+    return (
+        <>
+            <div>
+                <h2>The prophet once said: {quote} - Yeezus Christ </h2>
+                <button onClick={handleClick}>Hear more words to live by from the prophet.</button>
+            </div>
+        </>
+    );
+};
+
+const mapStateToProps = state =>{
+    return{
+        quote: state.quote,
+        isFetching: state.isFetching,
+        error: state.error
+    };
+};
+
+export default connect(mapStateToProps, {getQuote})(Quote)
