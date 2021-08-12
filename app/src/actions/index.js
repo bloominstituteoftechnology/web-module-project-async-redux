9236e50eda2d7e61e5fce3b527a82f9a445cfca7
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-08-11 18:30:28 -0600

    Finished.

---
+++ app/src/actions/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+
+export const fetchSongInfo = () => {
+  return (dispatch) => {
+    dispatch(fetchStart());
+
+    axios
+      .get("https://itunes.apple.com/search?term=MNQN")
+      .then((res) => {
+        dispatch({ type: FETCH_SUCCESS, payload: res.data.results });
+      })
+      .catch((err) => {
+        console.log("ERROR", err);
+        dispatch({ type: FETCH_FAIL, payload: err });
+      });
+  };
+};
+
+export const fetchStart = () => {
+  return { type: FETCH_START };
+};
+export const fetchSuccess = (songInfo) => {
+  return { type: FETCH_SUCCESS, payload: songInfo };
+};
+export const fetchFail = (err) => {
+  return { type: FETCH_FAIL, payload: err };
+};
