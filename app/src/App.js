50e1d61fd1ceab0f6fa6af62be7f80be0419a00f
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-11 17:19:40 -0700

    async-reduxcommit

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+//import Health from './components/Health'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Do u take care of your health?</h1>
+    </div>
+  );
+}
+
+export default App;
