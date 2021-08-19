1f99b6dcb62868b1fcbd90c51cc1a7207e83541b
andrewskr90 <andrewskr90@gmail.com>
2021-08-19 14:19:30 -0700

    push to update vercel branch

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
