fd3c36faf9ac0e258a74845e2cc99ff36989f792
Gabe Chilson <g.chilson34@gmail.com>
2021-08-24 21:42:08 -0600

    finished all mvp tasks

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,11 @@ function App() {
     <div className="App">
       <DogFactsForm />
       <DogFacts />
+      <img 
+      src={`https://www.vets4pets.com/siteassets/species/dog/two-dogs-running-with-balls.jpg?w=585&scale=down`} 
+      alt="Dogs playing together :)"
+      style={{ height: "300px", padding: "30px" }}
+      />
     </div>
   );
 }
--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -5,7 +5,7 @@ export const GET_DOG_FACTS = "Get Dog Facts";
 export const GOT_DOG_FACTS = "Got Dog Facts";
 
 export const getDogFacts = city => dispatch => {
-    axios.get(`https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all`).then(res => {
+    axios.get(`https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all`).then(res => { 
         console.log(res);
         const fact = res.data.fact;
         dispatch(gotDogFacts(fact)) 
