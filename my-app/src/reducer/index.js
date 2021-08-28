3dd0474b3de9090ead533a5155cb9a55f185b26e
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-08-27 20:56:30 -0500

    made action and reducer, but im a bit lost how to connect that with the components

--- my-app/src/reducer/index.js
+++ my-app/src/reducer/index.js
@@ -1,1 +1,23 @@
 import react from 'react'
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extensions'
+import { DICTIONARY_CALL, DICTIONARY_INFO } from '../actions/index';
+import axios from 'axios';
+
+const initialState = {
+	dictionary: []
+}
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case DICTIONARY_INFO:
+			return {
+				...state, dictionary: action.payload
+			}
+		default:
+			return state
+	}
+}
+
+export const store= createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
