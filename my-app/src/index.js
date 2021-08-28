85253028c6ebbbb507bec4cca112a491aab304d5
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-08-27 20:17:30 -0500

    installed bootstrap, still a bit lost with the whole project, but im gonna try

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstap/dist/cssbootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
