237dd96d293b20d1771da87085d1348c52fbf33c
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-09-08 13:26:55 -0600

    Initial commit. Added Food.js component

---
+++ redux-react/src/App.js
@@ -0,0 +1,16 @@
+//dependencies 
+import React from 'react';
+import './App.css';
+
+//components
+import Food from './components/Food';
+
+function App() {
+  return (
+    <div className="App">
+      <Food />
+    </div>
+  );
+}
+
+export default App;
