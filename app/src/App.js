32a923bab92a138b04debd414324a49b8ea8a486
Sebastian <sebas6292@gmail.com>
2021-09-09 21:39:53 -0700

    added axios

--- app/src/App.js
+++ app/src/App.js
@@ -2,20 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import BreweryList from './Components/BreweryList';
 // import data from './Data/brewery';
+import { useEffect } from 'react'; // step 21 
+import {fetchStart } from './actions'; //step 23 
 import BreweryForm from './Components/BreweryForm';
+import axios from 'axios';
 
 function App(props) {
 
   const { loading, brewery } = props //step 11
 
- 
+ useEffect(()=>{ //step 22 
+
+ }, [])
 
   return (
     <div className="App">
      <h1>Search for Drinks</h1>
 
      <BreweryForm /> 
-     
+
       {
         (error !== '') && <h3>{error}</h3>// step 14 
       }
@@ -34,4 +39,10 @@ const mapStateToProps = state => {//step 10
   }
 }
 
-export default connect(mapStateToProps)(App);//step 9 connect 
+const mapActionToProps = () => {//step 25 
+  return {
+    fetchStart
+  }
+}
+
+export default connect(mapStateToProps, {fetchStart})(App);//step 9 connect // step 24 put fetch start 
--- app/src/Reducer/index.js
+++ app/src/Reducer/index.js
@@ -1,5 +1,7 @@
 import data from './Data/brewery';// step 7 
 
+import { FETCH_START } from '../actions';// step 19 
+
 const initialState = {// step 5 
         brewery: data,
         loading: false,
@@ -8,6 +10,12 @@ const initialState = {// step 5
 
 const reducer =(state = initialState, action) => {// step 6
     switch(action.type) {
+        case FETCH_START: //* step 16 * // step 20 (take out the strings of FETCH_START)
+        return {
+            ...state,
+            loading: true, 
+            error: ''
+        }
         default: 
         return(state);
     }
