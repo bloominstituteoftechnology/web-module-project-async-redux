5382130dedbd51cd975fa089cef1028ec74dd134
natecheney-dev <natecheney@gmail.com>
2021-10-06 15:29:01 -0500

    finished mvp, bad styling though

---
+++ app/src/actions/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+
+
+export const getData = () => dispatch => {
+    dispatch(fetchStart());
+    
+    axios.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+        .then(resp=> {
+            dispatch(fetchSuccess(resp.data.data[Math.floor(Math.random() * resp.data.data.length)]));
+            console.log('axios', resp.data.data[Math.floor(Math.random() * resp.data.data.length)]);
+        })
+        .catch(err => {
+            dispatch(fetchFail(err));
+        });
+}
+
+
+export const fetchStart = ()=> {
+    return ({type:FETCH_START});
+}
+
+export const fetchSuccess = (data)=> {
+    return ({type:FETCH_SUCCESS, payload:data});
+}
+
+export const fetchFail = (err)=> {
+    return ({type:FETCH_FAIL, payload:err});
+}
---
+++ app/src/reducers/index.js
@@ -0,0 +1,43 @@
+
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL} from './../actions';
+
+const initialState = {
+    data: {
+        "ID Nation": "asd",
+        "Nation": "asd",
+         "ID Year": "456",
+        "Year": "123",
+        "Population": "asd",
+        "Slug Nation": "asd"
+    },
+    isFetching: false,
+    error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCH_START:
+            return{
+                ...state,
+                data: {},
+                isFetching: true,
+                error: ''
+            }
+        case FETCH_SUCCESS:
+            return{
+                ...state,
+                data: action.payload,
+                isFetching: false,
+                error: ''
+            }
+        case FETCH_FAIL:
+            return{
+                ...state,
+                data: {},
+                isFetching: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+}
