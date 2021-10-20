1d9ce4afeb84630707ea303ee9697edd5edc5d14
Owen  Damron <damronowen@yahoo.com>
2021-10-20 01:05:38 -0500

    Finished MVP

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PokeList from './components/PokeList';
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <div className="App">
+      <PokeList />
+    </div>
+  );
+}
+
+
+export default App;
