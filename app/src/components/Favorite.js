716b7eed444ddc284ab2bd89e8cf8959f705b7fd
djho57 <daniel.j.ho57@gmail.com>
2021-11-03 22:46:59 -0500

    mvp

---
+++ app/src/components/Favorite.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { deleteFavorite }from './../actions';
+import './Favorites.css'
+
+
+
+const Favorite = (props) => {
+
+console.log('props from fav',props)
+const handleClick = () =>{
+    props.dispatch(deleteFavorite(props.id))
+}
+
+  return (
+    <>
+      {
+      props.id &&  
+      <div className='favorite'>
+        <button onClick={handleClick}>-</button>
+        <p>{props.activity}</p>
+      </div>
+      }
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+   state
+  };
+};
+
+export default connect(mapStateToProps)(Favorite);
---
+++ app/src/components/Suggestion.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getSuggestion, addFavorite } from './../actions';
+import './Suggestions.css'
+
+
+const Suggestion = (props) => {
+
+  useEffect(()=>{
+   props.dispatch(getSuggestion())
+  },[]);
+
+  if (props.state.isFetching) {
+    return <p className='suggestion'>Fetching your suggestion...</p>;
+  }
+  if (props.state.error) {
+    return <h2 className='suggestion'>Error: {props.state.error}</h2>;
+  }
+  
+  const handleClick = () =>{
+    props.dispatch(getSuggestion())
+  }
+  const handleClick2 = () =>{
+    props.dispatch(addFavorite({activity:props.state.suggestion.activity, type:props.state.suggestion.type, id:props.state.suggestion.id}))
+    props.dispatch(getSuggestion())
+  }
+
+  return (
+    <>
+      <div className='suggestion'>
+        <div className="text">
+          <h2>Here's what you should do:</h2>
+          <p>{props.state.suggestion.activity}</p>
+          <p>Activity Type: {props.state.suggestion.type}</p>
+          <div className="buttons">
+            <button id='newSug' onClick={handleClick}>New Suggestion</button>
+            <button id='addFav' onClick={handleClick2}>Add Favorite</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+   state
+  };
+};
+
+export default connect(mapStateToProps)(Suggestion);
