9cbead1e6febb3874f987c35da17a0a8c3b5d823
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-05 20:38:25 -0700

    add Router to index.js

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider} from 'react-redux';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
@@ -21,7 +21,9 @@ console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+          <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
