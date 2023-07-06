ec1c2734d48b488d213f45e4539bc9f0166971de
DJ1 <jimmynoble604@gmail.com>
2023-07-06 13:14:50 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import cardReducer from './reducers/cardReducer';
+import { Provider } from 'react-redux';
+
+const store = configureStor({
+  reducer: {
+    card: cardReducer
+  }
+})
 
 ReactDOM.render(
-  <App />,
+  <Provider store = {store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
