f8c19343b19e72628eda19145fa90c4b3b35394f
Jomar Banting <shadowlover.05@gmail.com>
2023-08-21 20:29:25 -0400

    Setup the template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import data from "./data/gifs"
+import GifList from "./components/GifList"
 
 function App() {
+  const gifs = data;
+  const loading = false;
+  const error = ""
+
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Search for Gifs</h1>
+
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+    
+    {
+      loading ? <h3>We are loading</h3> : <GifList gifs={gifs}/>
+    }
     </div>
   );
 }
---
+++ src/data/gifs.js
@@ -0,0 +1,67 @@
+const data = [
+    {
+        "type": "gif",
+        "id": "VCyJwT1DZEWyqU95oS",
+        "url": "https://giphy.com/gifs/cat-ps4-dualshock-VCyJwT1DZEWyqU95oS",
+        "slug": "cat-ps4-dualshock-VCyJwT1DZEWyqU95oS",
+        "bitly_gif_url": "https://gph.is/g/amnBb6J",
+        "bitly_url": "https://gph.is/g/amnBb6J",
+        "embed_url": "https://giphy.com/embed/VCyJwT1DZEWyqU95oS",
+        "username": "",
+        "source": "",
+        "title": "Cat Reaction GIF",
+        "rating": "g",
+        "import_datetime": "2020-01-17 18:00:19",
+        "trending_datetime": "2021-01-11 23:00:11",
+        "images": {
+            "original": {
+                "url": "https://media2.giphy.com/media/VCyJwT1DZEWyqU95oS/giphy.gif?cid=181edf16scp0iyunaxref3ar399h15n8fwspse44yxffi4xy&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+                "hash": "bdbe2f7942302078932ab1744a5a3c72"
+            }
+        }
+    },
+    {
+        "type": "gif",
+        "id": "6VoDJzfRjJNbG",
+        "url": "https://giphy.com/gifs/cat-6VoDJzfRjJNbG",
+        "slug": "cat-6VoDJzfRjJNbG",
+        "bitly_gif_url": "http://gph.is/QFgPA0",
+        "bitly_url": "http://gph.is/QFgPA0",
+        "embed_url": "https://giphy.com/embed/6VoDJzfRjJNbG",
+        "username": "",
+        "source": "http://www.reddit.com/r/gifs/comments/23y94f/cat_attack/",
+        "title": "Cat Pounce GIF",
+        "rating": "g",
+        "import_datetime": "2014-04-25 14:46:21",
+        "trending_datetime": "2014-04-25 16:20:56",
+        "images": {
+            "original": {
+                "url": "https://media1.giphy.com/media/6VoDJzfRjJNbG/giphy.gif?cid=181edf16scp0iyunaxref3ar399h15n8fwspse44yxffi4xy&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+                "hash": "ce771abeb35ee5ea556e8ebd9c420c48"
+            }
+        }
+    },
+    {
+        "type": "gif",
+        "id": "2x0VePimPaFJDpGZ7H",
+        "url": "https://giphy.com/gifs/reaction-mood-2x0VePimPaFJDpGZ7H",
+        "slug": "reaction-mood-2x0VePimPaFJDpGZ7H",
+        "bitly_gif_url": "https://gph.is/2IW3ijl",
+        "bitly_url": "https://gph.is/2IW3ijl",
+        "embed_url": "https://giphy.com/embed/2x0VePimPaFJDpGZ7H",
+        "username": "",
+        "source": "http://cutecatgifs.com",
+        "title": "International Cat Day Cats GIF by MOODMAN",
+        "rating": "g",
+        "import_datetime": "2018-03-26 22:00:02",
+        "trending_datetime": "0000-00-00 00:00:00",
+        "images": {
+            "original": {
+                "url": "https://media3.giphy.com/media/2x0VePimPaFJDpGZ7H/giphy.gif?cid=181edf16scp0iyunaxref3ar399h15n8fwspse44yxffi4xy&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+                "hash": "89afe42ce75402a7bf8317cf78acd135"
+            }
+        }
+    }
+]
+
+export default data;
